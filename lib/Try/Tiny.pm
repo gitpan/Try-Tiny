caf73d5d7701bae3305ed3770fcca4f1df55aad1
Jesse Luehrs <doy@cpan.org>
2014-04-15 13:47:39 +0000

    Import of DOY/Try-Tiny-0.21 from CPAN.
    gitpan-cpan-distribution: Try-Tiny
    gitpan-cpan-version:      0.21
    gitpan-cpan-path:         DOY/Try-Tiny-0.21.tar.gz
    gitpan-cpan-author:       DOY
    gitpan-cpan-maturity:     released

--- lib/Try/Tiny.pm
+++ lib/Try/Tiny.pm
@@ -2,7 +2,7 @@ package Try::Tiny;
 BEGIN {
   $Try::Tiny::AUTHORITY = 'cpan:NUFFIN';
 }
-$Try::Tiny::VERSION = '0.20';
+$Try::Tiny::VERSION = '0.21';
 use 5.006;
 # ABSTRACT: minimal try/catch with proper preservation of $@
 
@@ -182,7 +182,7 @@ Try::Tiny - minimal try/catch with proper preservation of $@
 
 =head1 VERSION
 
-version 0.20
+version 0.21
 
 =head1 SYNOPSIS
 
